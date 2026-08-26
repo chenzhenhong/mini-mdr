9060d98d17bc2c9f21c82f1b601149e27becd084
sanne <2246467700@qq.com>
2026-08-26 20:09:56 +0800

    Fix FluentArgs: replace insert with set for fluent 0.16

--- src/i18n.rs
+++ src/i18n.rs
@@ -60,7 +60,8 @@ pub fn t_args(lang: Language, key: &str, args: &[(&str, &str)]) -> String {
         };
         let mut fluent_args: FluentArgs = FluentArgs::with_capacity(args.len());
         for (k, v) in args {
-            fluent_args.insert((*k).to_string(), (*v).to_string().into());
+            fluent_args
+                .set::<std::string::String, fluent::FluentValue>((*k).to_string(), (*v).to_string().into());
         }
         let mut errors = vec![];
         bundle
